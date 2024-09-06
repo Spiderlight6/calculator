b57ae6a117410ab3ebdd30711f4ab9c5db46da01
Spiderlight6 <170469796+Spiderlight6@user.noreply.github.com>
2024-09-06 20:00:19 +0200

    Changes to be committed: 	modified:   index.html 	modified:   index.js 	modified:   style.css

---
+++ index.js
@@ -0,0 +1,49 @@
+
+let firstNumber;
+let theOperator;
+let secondNumber;
+
+const display = document.querySelector("#display");
+let showResult;
+
+function showDisplay(v){
+    showResult = v;
+    display.textContent = showResult;
+
+}
+
+
+const adding = function(x, y){
+    return x + y;
+}
+
+const subtracting = function(x, y){
+    return x - y;
+}
+
+const multiplying = function(x, y){
+    return x * y;
+}
+
+const dividing = function(x, y){
+    return Math.ceil(x / y);
+}
+
+function operate(theOperator, firstNumber, secondNumber){
+    if(theOperator === "+" ){
+        return adding(firstNumber, secondNumber);
+    }
+    else if(theOperator === "-"){
+        return subtracting(firstNumber, secondNumber);
+    }
+    else if(theOperator === "*"){
+        return multiplying(firstNumber, secondNumber);
+    }
+    else if(theOperator === "/"){
+        return dividing(firstNumber, secondNumber);
+    }
+    else{
+        return "hello";
+    }
+}
+
